d24fca8933cecf886220b47dd1763c3b4845925c
Hugo Eusebio <2180607@my.ipleira.pt>
2020-11-11 16:15:46 +0000

    Teste5Hugo

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -15,7 +15,7 @@ $this->title = 'My Yii Application';
         <table>
             <tbody>
                 <tr>
-                    <td> </td>
+                    <td>tou</td>
                 </tr>
             </tbody>
         </table>
