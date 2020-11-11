518182882435790ff0265f0065eab246154f5494
David Simões <2170369@my.ipleiria.pt>
2020-11-11 15:53:46 +0000

    Teste 3

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -15,7 +15,7 @@ $this->title = 'My Yii Application';
         <table>
             <tbody>
                 <tr>
-                    <td></td>
+                    <td> </td>
                 </tr>
             </tbody>
         </table>
